791b4dadf9e8c7032522a52223d2b2ca993c74bd
KAPP Arnaud <kapp.arno@gmail.com>
2012-04-03 18:46:37 +0200

    fix write_to_sock to avoid overflow

--- src/write.c
+++ src/write.c
@@ -5,9 +5,10 @@
 ** Login   <kapp_a@epitech.net>
 ** 
 ** Started on  Tue Apr  3 16:13:44 2012 arnaud kapp
-** Last update Tue Apr  3 16:59:46 2012 arnaud kapp
+** Last update Tue Apr  3 18:45:30 2012 arnaud kapp
 */
 
+#include	<stdio.h>
 #include	<unistd.h>
 #include	"tcpclient.h"
 
@@ -17,7 +18,8 @@ void		write_to_sock(t_tcp_client *c)
   int		r;
 
   r = rgbuf_r_available(c->sock.wbuffer);
-  rgbuf_read(c->sock.wbuffer, buffer, sizeof(buffer));
+  r = r > 512 ? 512 : r;
+  rgbuf_read(c->sock.wbuffer, buffer, r);
   if (r)
     write(c->sock.fd, (char *)buffer, r);
 }
